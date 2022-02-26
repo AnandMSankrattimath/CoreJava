5f5e1c685f6ab6e6de35f018589bacb397ad91f2
AnandMSankrattimath <anandmsmath.xworkz@gmail.com>
2022-02-26 10:35:34 +0530

    Datatypes Uploaded

---
+++ DATATYPES/furniture.java
@@ -0,0 +1,29 @@
+class table {
+	public static void main(String [] arg)
+	    {
+		byte nooflegs= 65;
+			System.out.println(nooflegs); // output will be 65
+
+		short s = 30000;
+			System.out.println(s);	// output will be 30000
+
+		int i = 123456789;
+			System.out.println(i);	// output will be 123456789
+
+		long l = 123456789078963l;
+			System.out.println(l);	// output will be 123456789078963
+
+		char c = 'A';
+			System.out.println(c);	// output will be A
+
+		float f = 1.123456123f;
+			System.out.println(f);	// output will be 1.1234561
+
+		double d = 1.123456789456789123d;
+			System.out.println(d);	// output will be 1.123456789456789
+
+		boolean b = true;
+			System.out.println(b);	// output will be true
+
+}
+}
---
+++ DATATYPES/okok.java
@@ -0,0 +1,13 @@
+
+class anand {
+    public static void main(String [] args)
+    {
+
+    
+    System.out.println("HI HELLO ");
+    System.out.println("hello world");
+    System.out.println("okokokokok");
+
+    }
+
+}
