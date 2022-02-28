89ed7775c8b90097967a182abdb218a8baae7d23
AnandMSankrattimath <anandmsmath.xworkz@gmail.com>
2022-02-28 11:41:26 +0530

    Devendra sir program is uploaded

---
+++ DevendraSinghaChavhanSir/variableExample/JabraElite2.java
@@ -0,0 +1,16 @@
+class JabraElite2{
+	public static void main(String [] args){
+	String Brand ="Jabra";
+	String ConnectorType="Wireless";
+	String Colour="Navy";
+	double price = 3900.00;
+	
+	System.out.println("Brand is "+Brand);
+	System.out.println("ConnectorType is "+ConnectorType);
+	System.out.println("Colour is " +Colour);
+	System.out.println("price is "+price);
+	
+	
+	
+}
+}
